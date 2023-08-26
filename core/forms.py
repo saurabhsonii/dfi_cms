d111f4935d063d9fade20da66f6a869c1b5131c1
mannu32 <sahumanmohan360@gmail.com>
2023-08-26 13:48:04 +0530

    update user side

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,11 @@
 from django import forms
-
+# from .models import CustomUser
 
 class LoginForm(forms.Form):
     email = forms.EmailField(label='Email')
     password = forms.CharField(widget=forms.PasswordInput, label='Password')
+
+# class CustomUser(forms.ModelForm):
+#     class Meta:
+#         model = CustomUser
+#         fields = ['email', 'name','phone_number', 'address','profile_image']
--- core/views.py
+++ core/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+# custom 404 view
+def custom_404(request, exception):
+    return render(request, '404/404.html', status=404)
+    
 # Create your views here.
 
-
+@login_required(login_url='login')
 def index(request):
     return render(request, "dashboard/index.html")
 
@@ -28,3 +35,8 @@ def login_view(request):
         form = LoginForm()
 
     return render(request, 'dashboard/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
