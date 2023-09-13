7238c96860e19f93edaa6ecb40826eb437fe6b86
saurabhsonii <saurabhsoni.dev@gmail.com>
2023-09-14 00:45:55 +0530

    commit for tables

--- dfi_cms/settings.py
+++ dfi_cms/settings.py
@@ -119,13 +119,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
-if DEBUG:
-    STATICFILES_DIRS = [BASE_DIR / 'static',]
-else:
-    STATIC_ROOT = BASE_DIR / 'static'
+# if DEBUG:
+#     STATICFILES_DIRS = [BASE_DIR / 'static',]
+# else:
+#     STATIC_ROOT = BASE_DIR / 'static'
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
